e414af3f3528affc91e839eccec95bf16f101a86
RahulSarkar <bluephoenix00995@gmail.com>
2025-07-12 18:45:17 +0530

    feat: store bug fixed

--- salesforce_management/salesforce_management/doctype/store/store.py
+++ salesforce_management/salesforce_management/doctype/store/store.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class Store(Document):
 	def before_save(self):
@@ -20,17 +21,45 @@ class Store(Document):
 	def before_cancel(self):
 		self.status = "Cancelled"
 
+	# def after_insert(self):
+	# 	_create_warehouse(self)
+	# 	# Set Created to Details
+	# 	employee_details = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "*")
+	# 	if not employee_details: return
+	# 	frappe.db.set_value("Store", self.name, 'created_by_employee', employee_details.get("name"))
+	# 	frappe.db.set_value("Store", self.name, 'created_by_employee_name', employee_details.get("employee_name"))
+	# 	frappe.db.set_value("Store", self.name, 'created_by_employee_designation', employee_details.get("designation"))
+	# 	reports_to_details = frappe.db.get_value("Employee", {"name": employee_details.get("reports_to")}, "*")
+	# 	frappe.db.set_value("Store", self.name, 'reports_to_name', reports_to_details.get("employee_name"))
+	# 	frappe.db.set_value("Store", self.name, 'reports_to_designation', reports_to_details.get("designation"))
+	# 	self.reload()
 	def after_insert(self):
 		_create_warehouse(self)
 		# Set Created to Details
 		employee_details = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "*")
-		if not employee_details: return
+		if not employee_details: 
+			return
+		
 		frappe.db.set_value("Store", self.name, 'created_by_employee', employee_details.get("name"))
 		frappe.db.set_value("Store", self.name, 'created_by_employee_name', employee_details.get("employee_name"))
 		frappe.db.set_value("Store", self.name, 'created_by_employee_designation', employee_details.get("designation"))
-		reports_to_details = frappe.db.get_value("Employee", {"name": employee_details.get("reports_to")}, "*")
-		frappe.db.set_value("Store", self.name, 'reports_to_name', reports_to_details.get("employee_name"))
-		frappe.db.set_value("Store", self.name, 'reports_to_designation', reports_to_details.get("designation"))
+		
+		# Check if reports_to exists before querying
+		reports_to = employee_details.get("reports_to")
+		if reports_to:
+			reports_to_details = frappe.db.get_value("Employee", {"name": reports_to}, "*")
+			if reports_to_details:
+				frappe.db.set_value("Store", self.name, 'reports_to_name', reports_to_details.get("employee_name"))
+				frappe.db.set_value("Store", self.name, 'reports_to_designation', reports_to_details.get("designation"))
+			else:
+				# reports_to employee doesn't exist in database
+				frappe.db.set_value("Store", self.name, 'reports_to_name', "")
+				frappe.db.set_value("Store", self.name, 'reports_to_designation', "")
+		else:
+			# No reports_to assigned to this employee
+			frappe.db.set_value("Store", self.name, 'reports_to_name', "")
+			frappe.db.set_value("Store", self.name, 'reports_to_designation', "")
+		
 		self.reload()
 	
 	def on_trash(self):
